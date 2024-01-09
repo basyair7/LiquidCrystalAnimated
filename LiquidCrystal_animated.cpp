97a87945af050f35fc4b860e4c32c844382499f6
Fathul <basyair.fathul@gmail.com>
2024-01-09 16:24:12 +0700

    Add new Library LiquidCrystal MOD

---
+++ LiquidCrystal_animated.cpp
@@ -0,0 +1,44 @@
+// Based on the work by DFRobot
+
+#include "Arduino.h"
+#include "LiquidCrystal_animated.h"
+
+LiquidCrystal_animated::LiquidCrystal_animated(){
+    // Nothing
+}
+
+String LiquidCrystal_animated::Scroll_LCD_Left(String StrDisplay) {
+    String result;
+    String StrProcess = "                " + StrDisplay + "                ";
+    result = StrProcess.substring(Li,Lii);
+    Li++;
+    Lii++;
+    if (Li>StrProcess.length()){
+        Li=16;
+        Lii=0;
+    }
+    return result;    
+}
+
+String LiquidCrystal_animated::Scroll_LCD_Right(String StrDisplay) {
+    String result;
+    String StrProcess = "                " + StrDisplay + "                ";
+    if (Rii<1){
+        Ri  = StrProcess.length();
+        Rii = Ri-16;
+    }
+    result = StrProcess.substring(Rii,Ri);
+    Ri--;
+    Rii--;
+    return result;
+}
+
+void LiquidCrystal_animated::Clear_Scroll_LCD_Left() {
+    Li = 16;
+    Lii = 0;
+}
+
+void LiquidCrystal_animated::Clear_Scroll_LCD_Right() {
+    Ri = -1;
+    Rii = -1;
+}
