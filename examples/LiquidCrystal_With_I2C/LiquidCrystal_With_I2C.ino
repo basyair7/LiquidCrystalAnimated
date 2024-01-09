da28e8ee818139b818ff9012aa5d1c5929b9a693
Fathul <basyair.fathul@gmail.com>
2024-01-09 16:28:09 +0700

    Add new Library LiquidCrystal MOD

---
+++ examples/LiquidCrystal_With_I2C/LiquidCrystal_With_I2C.ino
@@ -0,0 +1,59 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_animated.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_animated ChrAnimated;
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i <= 12; i++) {
+    lcd.setCursor(0, 0);
+    lcd.print(ChrAnimated.Scroll_LCD_Right("Basyair7"));
+    delay(350);
+  }
+  
+  for (int i = 0; i <= 50; i++) {
+    lcd.setCursor(0, 1);
+    lcd.print(ChrAnimated.Scroll_LCD_Left("Cara Membuat Text Berjalan...."));
+    delay(350);
+  }
+
+  ChrAnimated.Clear_Scroll_LCD_Left();
+
+  for (int i = 16; i <= 30; i++) {
+    lcd.setCursor(0, 0);
+    lcd.print(ChrAnimated.Scroll_LCD_Right("Basyair7"));
+    delay(350);
+  }
+
+  ChrAnimated.Clear_Scroll_LCD_Right();
+  
+  for (int i = 0; i <= 12; i++) {
+    lcd.setCursor(0, 0);
+    lcd.print(ChrAnimated.Scroll_LCD_Right("Basyair7"));
+    delay(350);
+  }
+
+  for (int i = 0; i <= 73; i++) {
+    lcd.setCursor(0, 1);
+    lcd.print(ChrAnimated.Scroll_LCD_Left("Selamat Datang Di Basyair7!!..."));
+    delay(350);
+  }
+
+  ChrAnimated.Clear_Scroll_LCD_Left();
+
+  for (int i = 13; i <= 24; i++) {
+    lcd.setCursor(0, 0);
+    lcd.print(ChrAnimated.Scroll_LCD_Right("Basyair7"));
+    delay(350);
+  }
+  ChrAnimated.Clear_Scroll_LCD_Right();
+}
